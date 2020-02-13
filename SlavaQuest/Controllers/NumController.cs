28132ab61b749f5f735b785dab470240f031ca3b
NYKOLYA <nynksolo@gmail.com>
2020-02-13 15:37:59 +0200

    Add project files.

---
+++ SlavaQuest/Controllers/NumController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SlavaQuest.Models;
+
+namespace SlavaQuest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NumController : ControllerBase
+    {
+        Student MyStudent = null;
+
+        public NumController()
+        {
+            //MyStudent = new Student(Guid.NewGuid(),1,"nick");
+        }
+
+        [HttpGet("G")]
+        public ActionResult<string> GetString()
+        {
+            return Ok(MyStudent);
+        }
+
+        [HttpPost("PO")]
+        public ActionResult CreateString([FromQuery]Student student)
+        {
+            if (student != null)
+            {
+                return BadRequest("invalid data");
+            }
+
+            MyStudent = student;
+            MyStudent.Id = Guid.NewGuid();
+
+            return Ok();
+        }
+        
+        [HttpPut("PU")]
+        public ActionResult<string> UpdateString(byte age = 0, string name = "")
+        {
+            if (age != 0)
+            {
+                MyStudent.Age = age;
+            }
+
+            if (name != "")
+            {
+                MyStudent.Name = name;
+            }
+
+            if (age == 0 && name == "")
+            {
+                return BadRequest("nothing to update");
+            }
+
+            return Ok(MyStudent);
+        }
+
+        [HttpDelete("D")]
+        public ActionResult RemoveString()
+        {
+            MyStudent = null;
+
+            return Ok();
+        }
+    }
+}
---
+++ SlavaQuest/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SlavaQuest.Models
+{
+    public class Student
+    {
+        public Guid Id { get; set; }
+        public byte Age { get; set; }
+        public string Name { get; set; }
+        public Student()
+        {
+
+        }
+        public Student(Guid Id, Byte age, String name)
+        {
+            this.Id = Id;
+            this.Age = age;
+            this.Name = name;
+        }
+    }
+}
